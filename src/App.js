da1fd3a031e1acaafb9c54196a82589d1b55e4c9
Julio Reyes <jcreyes@g.clemson.edu>
2023-01-16 14:11:06 -0500

    Set up font families, header positioning
    CSS is currently the bane of my existence

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Header from './Components/Header/Header'
+import './global.css'
 // Importing our components
 
 
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import './Header.css'
 
 const Header = () => {
   return (
-    <div>This is a header</div>
+    <div className="header">
+        <div className="header top">
+            <div className="top-container">
+                <div className="top-logo">
+                    <h5>LEVIATHAN</h5>
+                </div>
+            </div>
+        </div>
+        <div className="header middle">
+            <div className="middle-container">
+                MIDDLE HEADER
+            </div>
+        </div>
+        <div className="header bottom">
+            <div className="bottom-container">
+                BOTTOM HEADER
+            </div>
+        </div>
+    </div>
   )
 }
 
