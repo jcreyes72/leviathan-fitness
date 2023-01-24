b4dd0bf71b3b3dc5cb583227c4e0997fe9731697
Julio Reyes <jcreyes@g.clemson.edu>
2023-01-24 12:33:37 -0500

    Adding promo text to large image
    Need to fix positioning issues with our "shop weightlifting" button

--- src/Components/FeaturedImage/FeaturedImage.jsx
+++ src/Components/FeaturedImage/FeaturedImage.jsx
@@ -8,7 +8,21 @@ const FeaturedImage = () => {
         <div className="big-poster">
             <img src={posterImage} alt="" className='main-image' />
             <div className="big-text-container">
-                <h3 className='big-text'>TRANSFORM THE WAY YOU TRAIN</h3>
+                <h3 className='big-text'>TRANSFORM <br/> THE WAY <br/> YOU TRAIN</h3>
+            </div>
+            <div className="small-text-container">
+                <h3 className='small-text'>
+                   <span>YOUR <br/></span>
+                </h3>
+                <h3 className="small-text-red">
+                   <span> NEW YEARS RESOLUTION <br/></span>
+                </h3>
+                <h3 className="small-text">
+                    <span> WON'T KNOW WHAT HIT IT </span>
+                </h3>
+            </div>
+            <div className="shop-now-container">
+                <a href="/" className='shop-weightlifting'>SHOP WEIGHTLIFTING</a>
             </div>
         </div>
     </div>
