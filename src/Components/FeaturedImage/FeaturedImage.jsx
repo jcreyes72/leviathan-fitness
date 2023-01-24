66863274027c466587fe5e1daaaddd236cf363b8
Julio Reyes <jcreyes@g.clemson.edu>
2023-01-24 10:56:12 -0500

    Added home image

--- src/Components/FeaturedImage/FeaturedImage.jsx
+++ src/Components/FeaturedImage/FeaturedImage.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import './FeaturedImage.css'
+import posterImage from "../../Images/cover.jpg"
 
 const FeaturedImage = () => {
   return (
     <div className="poster-wrapper">
         <div className="big-poster">
-
+            <img src={posterImage} alt="" className='main-image' />
         </div>
     </div>
   )
