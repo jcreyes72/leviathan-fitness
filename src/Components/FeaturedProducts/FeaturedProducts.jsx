22b1fe4ba6c056071e038d3815b3530e9c016fb3
Julio Reyes <jcreyes@g.clemson.edu>
2023-01-25 15:37:45 -0500

    Working on our featured products section

--- src/Components/FeaturedProducts/FeaturedProducts.jsx
+++ src/Components/FeaturedProducts/FeaturedProducts.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import "./FeaturedProducts.css"
 
 const FeaturedProducts = () => {
   return (
-    <div>here are the featured products!!!</div>
+    <div className="section-wrapper">
+        <div className="tab-header">
+            <h2 className='section-name'>FEATURED PRODUCTS</h2>
+            <div className="fill"></div>
+            <div className="tabs-select">
+                <button>button1</button>
+                <button>button2</button>
+                <button>button3</button>
+                <button>button4</button>
+                <button>button5</button>
+            </div>
+        </div>
+    </div>
+    
   )
 }
 
