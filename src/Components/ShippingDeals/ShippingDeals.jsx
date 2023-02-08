f07dfd404382f33f5149973685015e4c51aa2448
Julio Reyes <jcreyes@g.clemson.edu>
2023-02-08 15:48:55 -0500

    Implemented some media queries

--- src/Components/ShippingDeals/ShippingDeals.jsx
+++ src/Components/ShippingDeals/ShippingDeals.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './ShippingDeals.css'
 import dailyDeals from "../../Images/shipping-deals.jpg"
+import dailyDeals2 from "../../Images/shipping-deals-small.png"
+import dailyDeals3 from "../../Images/shipping-deals-smallest.png"
 
 const ShippingDeals = () => {
   return (
@@ -9,8 +11,14 @@ const ShippingDeals = () => {
             <h2 className='section-name'>DAILY SHIPPING DEALS</h2>
         </div>
         <div className="grey-line"></div>
-        <div className="deals-container">
+        <div className="deals-container big">
             <img src={dailyDeals} alt="" className='daily-deals'/>
+        </div>
+        <div className="deals-container small">
+            <img src={dailyDeals2} alt="" className='daily-deals'/>
+        </div>
+        <div className="deals-container smallest">
+            <img src={dailyDeals3} alt="" className='daily-deals'/>
         </div>
     </div>
   )
